117b60fc5e79465387ed76c547f104f9c4d3918c
Timothy Vernon <timothy.n.vernon@gmail.com>
2018-10-03 12:21:41 -0400

    feature(recent-posts): Add recent posts to sidebar on post pages

--- src/components/RecentPosts.js
+++ src/components/RecentPosts.js
@@ -2,20 +2,40 @@ import React from 'react'
 import Link from 'gatsby-link'
 import { css } from 'emotion'
 import styled from 'react-emotion'
+import { grey, orange } from '../styles/colors'
 
 const Aside = styled('aside')`
-  padding: 20px;
-  background-color: #efefef;
-  border-right: 1px solid #ddd;
+  padding: 20px 40px;
 `
 
-const Heading = styled('p')`
-  font-size: 26px;
+const Heading = styled('h2')`
+  margin-top: -20px;
+  padding-bottom: 12pt;
+  font-size: 20pt;
+  border-bottom: 1px solid ${grey};
 `
 
 const test = css`
   display: block;
   padding-bottom: 10px;
+  text-decoration: none;
+  color: ${grey};
+  background-color: white;
+  padding: 20px;
+  margin-bottom: 40px;
+  border-bottom: 5px solid ${orange};
+`
+
+const titleText = css`
+  margin-top: 0;
+  font-weight: 500;
+  text-decoration: underline;
+`
+
+const smallText = css`
+  font-size: 12pt;
+  margin-bottom: 0;
+  font-style: italic;
 `
 
 const RecentPosts = props => {
@@ -24,7 +44,7 @@ const RecentPosts = props => {
     <Aside>
       <Heading>Recent Posts</Heading>
       {posts.map(post => {
-        const { date, path, title } = post.node.frontmatter
+        const { date, path, title, description } = post.node.frontmatter
         const { id } = post.node
         return (
           <Link
@@ -34,12 +54,45 @@ const RecentPosts = props => {
             to={path}
             key={id}
           >
-            <p>{title}</p>
-            <p>{date}</p>
+            <p
+              className={css`
+                ${titleText};
+              `}
+            >
+              {title}
+            </p>
+            <p
+              className={css`
+                ${smallText};
+              `}
+            >
+              {date}
+            </p>
+            <p
+              className={css`
+                ${smallText};
+              `}
+            >
+              {description}
+            </p>
           </Link>
         )
       })}
-      <Link to="/blog">All Posts</Link>
+      <Link
+        to="/"
+        className={css`
+          ${test};
+        `}
+      >
+        <p
+          className={css`
+            ${titleText};
+            margin-bottom: 0;
+          `}
+        >
+          All Posts
+        </p>
+      </Link>
     </Aside>
   )
 }
